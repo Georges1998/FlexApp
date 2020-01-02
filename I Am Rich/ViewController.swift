b759bbccf73a4e3de9fe90058d23780678d6b25b
George Kandalaft <georgekandalaft@Georges-MacBook-Pro.local>
2020-01-02 14:55:48 -0500

    The App

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by George Kandalaft on 2019-12-31.
+//  Copyright © 2019 George Kandalaft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
